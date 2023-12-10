4746a4131a386657b154b25efb930746957bac3a
Laurin McKenna <laurin.mckenna@oneidentity.com>
2023-12-10 00:51:27 -0900

    Day2 finished puzzle 2

--- day2 - GameCube/cube_conundrum.py
+++ day2 - GameCube/cube_conundrum.py
@@ -14,8 +14,61 @@ def calculateCube(filename, red_cube_num, green_cube_num, blue_cube_num):
                             "blue": blue_cube_num}
         sum += calculateLine(line, allowed_maximums)
         
+    print("Sum:", sum)
+    file.close()
+
+def calculateCubeMin(filename):
+    file = open(filename, "r") 
+
+    sum = 0
+    while True: 
+        line = file.readline() 
+
+        if not line: 
+            break 
+        line = line.strip() 
+        sum += calculateLineMin(line)
+        
     print("Sum: ", sum)
     file.close()
+
+def calculateLineMin(line):
+    stripped_line = line.replace(" ", "")
+    stripped_line = stripped_line[6:]
+
+    #Find max of each color 
+    maximums = {"red": 0, "blue": 0, "green": 0}
+    num = ""
+    STATE = ""
+    for char in stripped_line: 
+        try: 
+            int(char)
+            STATE = 'NUM'
+            num += char 
+        except: 
+            if char == "b":
+                STATE = 'BLUE'
+            elif char == "g": 
+                STATE= 'GREEN'
+            elif char == "," or char == ";": 
+                STATE = 'PUNCT'
+                continue 
+            
+            if char == "d": 
+                maximums["red"] = max(int(num), maximums["red"])
+                num = ""
+                STATE = ""
+            elif char == "n": 
+                maximums["green"] = max(int(num), maximums["green"])
+                num = ""
+                STATE = ""
+
+            elif char == "e" and STATE == "BLUE": 
+                maximums["blue"] = max(int(num), maximums["blue"])
+                num = ""
+                STATE = ""
+                
+    return maximums["red"] * maximums["green"] * maximums["blue"]
 
 def calculateLine(line, allowed_maximums):
     #Get the Game ID 
@@ -65,5 +118,6 @@ def calculateLine(line, allowed_maximums):
 
 
 if __name__ == "__main__": 
-    print("Calibrating...")
+    print("Calculating...")
     calculateCube("dataset1.txt", 12, 13, 14)
+    calculateCubeMin("dataset1.txt")
--- day2 - GameCube/test.py
+++ day2 - GameCube/test.py
@@ -2,3 +2,5 @@ from cube_conundrum import *
 
 print("Running tests....")
 calculateCube("testdataset.txt", 12, 13, 14)
+
+calculateCubeMin("testdataset.txt")
