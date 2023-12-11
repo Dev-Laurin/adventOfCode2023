203e145d4b544732a13124d9f0e684ed3d49aacb
Laurin McKenna <laurin.mckenna@oneidentity.com>
2023-12-11 13:18:59 -0900

    Day3 rewriting to read all input at once

--- day3 - Gear Ratios/counting_engine_parts.py
+++ day3 - Gear Ratios/counting_engine_parts.py
@@ -7,16 +7,14 @@ def countEngineParts(filename):
     index = 0 
     STATE = ""
     num_str = ""
-    num_indices = []
     touching_pos = []
-    while True: 
-        line = file.readline() 
-
-        if not line: 
-            break 
-        line = line.strip() 
-        parseLine(line, symbol_pos, number_pos, 
-                  index, STATE, num_str, num_indices, touching_pos)
+ 
+    string = file.read()
+    if not string: 
+        return  
+    
+    parseFile(string, symbol_pos, number_pos, 
+                index, STATE, num_str, touching_pos)
 
     sum = sumTouchingNumbers(touching_pos, number_pos)
         
@@ -33,8 +31,8 @@ def sumTouchingNumbers(touchingPositions, number_pos):
     return sum 
 
 
-def calculateTouchingPositions(line, symbol_pos, touching_positions):
-    length = len(line)
+def calculateTouchingPositions(data, symbol_pos, touching_positions):
+    length = len(data)
     lastIndex = length - 1
     for s_pos in symbol_pos: 
         if calculateIfValid(s_pos - length, lastIndex):
@@ -71,28 +69,30 @@ def calculateTouchingNumbers(positions, number_positions):
     num_list = []
 
     print("positions: ", positions)
-    print("num pos:",number_positions)
+    print("num pos:", number_positions)
 
     for p in positions: 
-        print("Position: ", p)
+        #print("Position: ", p)
         for index, num in enumerate(number_positions): 
-            print("number_list num:", num, "index:", index)
+            #print("number_list num:", num, "index:", index)
             for num_pos in number_positions[index][1]:
-                print("number position: ", num_pos)
+                #print("number position: ", num_pos)
                 if num_pos == p: 
-                    print("number touches a symbol")
+                    #print("number touches a symbol")
                     num_list.append(number_positions[index][0])
 
     return num_list 
 
-def parseLine(line, symbol_pos, number_pos, index, STATE, num_str, num_indices, touching_pos):
+def parseFile(data, symbol_pos, number_pos, index, STATE, num_str, touching_pos):
     #Find symbol and number positions
-    for char in line: 
+    num_indices = []
+    for char in data: 
         #A Symbol is not a period and not a number
         if char == ".":
             if STATE == 'NUM': 
                 number_pos.append([num_str, num_indices])
                 num_str = ""
+                num_indices = []
             STATE = 'PERIOD'
             index += 1 
             continue 
@@ -101,12 +101,14 @@ def parseLine(line, symbol_pos, number_pos, index, STATE, num_str, num_indices, 
             STATE = 'NUM'
             num_str += char 
             num_indices.append(index)
+            print("Number:", char, "found at index:", index)
             index += 1 
             continue 
         except: 
             if STATE == 'NUM': 
                 number_pos.append([num_str, num_indices])
                 num_str = ""
+                num_indices = []
             STATE = 'SYMBOL'
             num_str = ""
             symbol_pos.append(index) 
@@ -114,7 +116,7 @@ def parseLine(line, symbol_pos, number_pos, index, STATE, num_str, num_indices, 
         index += 1 
 
     #Calculate touching symbol positions
-    calculateTouchingPositions(line, symbol_pos, touching_pos)
+    calculateTouchingPositions(data, symbol_pos, touching_pos)
 
 
 if __name__ == "__main__": 
