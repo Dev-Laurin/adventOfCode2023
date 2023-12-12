64f5460f5131b6bc0b0d694b25498ee5dc9740e2
Laurin McKenna <laurin.mckenna@oneidentity.com>
2023-12-11 20:54:18 -0900

    Day3 puzzle 2 solved

--- day3 - Gear Ratios/counting_engine_parts.py
+++ day3 - Gear Ratios/counting_engine_parts.py
@@ -7,7 +7,6 @@ def countEngineParts(filename):
     index = 0 
     STATE = ""
     num_str = ""
-    touching_pos = []
  
     string = file.read()
     if not string: 
@@ -16,101 +15,97 @@ def countEngineParts(filename):
     string = string.replace("\n", "")
     
     parseFile(string, symbol_pos, number_pos, 
-                index, STATE, num_str, touching_pos, lineLength)
-    
-    print("Touching Pos:", touching_pos)
+                index, STATE, num_str, lineLength)
 
-    sum = sumTouchingNumbers(touching_pos, number_pos)
-        
+    sum = sumTouchingNumbers(symbol_pos, number_pos)
     print("Sum:", sum)
     file.close()
 
     return sum 
 
+def calculateGearRatio(li):
+    gearRatio = 1 
+    for num in li: 
+        gearRatio *= num 
+    return gearRatio 
+
 def sumTouchingNumbers(touchingPositions, number_pos):
     #Calculate which numbers touch a symbol
-    number_list = calculateTouchingNumbers(touchingPositions, number_pos)
-    print("Numbers to sum: ", number_list)
+    calculateTouchingNumbers(touchingPositions, number_pos)
+
     sum = 0
-    for num in number_list: 
-        sum += int(num)
+    for sym in touchingPositions: 
+        if len(sym[2]) > 1: 
+            sum += calculateGearRatio(sym[2]) 
+
     return sum 
 
 
-def calculateTouchingPositions(data, symbol_pos, touching_positions, length):
+def calculateTouchingPositions(data, s_pos, length):
     lastIndex = len(data)
-    print("lastIndex: ", lastIndex)
-    print("length: ", length)
-    for s_pos in symbol_pos: 
-        up = s_pos - length
-        down = s_pos + length
-        left = s_pos - 1
-        right = s_pos + 1
-        upper_right = s_pos - length + 1
-        upper_left = s_pos - length - 1
-        lower_right = s_pos + length + 1
-        lower_left = s_pos + length - 1
-        if calculateIfValid(up, lastIndex):
-            #Up
-            touching_positions.append(up)
-        if calculateIfValid(down, lastIndex):
-            #Down
-            touching_positions.append(down)
-        if calculateIfValid(left, lastIndex):
-            #Left
-            touching_positions.append(left)
-        if calculateIfValid(right, lastIndex):
-            #Right
-            touching_positions.append(right)
-        if calculateIfValid(upper_right, lastIndex):
-            #Upper Right Diagonal
-            touching_positions.append(upper_right)
-        if calculateIfValid(upper_left, lastIndex):
-            #Upper Left Diagonal
-            touching_positions.append(upper_left)
-        if calculateIfValid(lower_right, lastIndex):
-            #Lower Right Diagonal
-            touching_positions.append(lower_right)
-        if calculateIfValid(lower_left, lastIndex):
-            #Lower Left Diagonal
-            touching_positions.append(lower_left)
+    up = s_pos - length
+    down = s_pos + length
+    left = s_pos - 1
+    right = s_pos + 1
+    upper_right = s_pos - length + 1
+    upper_left = s_pos - length - 1
+    lower_right = s_pos + length + 1
+    lower_left = s_pos + length - 1
+
+    touching_positions = {}
+    if calculateIfValid(up, lastIndex):
+        #Up
+        touching_positions[up] = up 
+    if calculateIfValid(down, lastIndex):
+        #Down
+        touching_positions[down] = down 
+    if calculateIfValid(left, lastIndex):
+        #Left
+        touching_positions[left] = left 
+    if calculateIfValid(right, lastIndex):
+        #Right
+        touching_positions[right] = right 
+    if calculateIfValid(upper_right, lastIndex):
+        #Upper Right Diagonal
+        touching_positions[upper_right] = upper_right 
+    if calculateIfValid(upper_left, lastIndex):
+        #Upper Left Diagonal
+        touching_positions[upper_left] = upper_left
+    if calculateIfValid(lower_right, lastIndex):
+        #Lower Right Diagonal
+        touching_positions[lower_right] = lower_right
+    if calculateIfValid(lower_left, lastIndex):
+        #Lower Left Diagonal
+        touching_positions[lower_left] = lower_left
+
+    return touching_positions
 
 def calculateIfValid(position, lastIndex): 
-    print("checking if position is valid: ", position, " lastIndex: ", lastIndex)
     if position < 0 or position > lastIndex: 
         return False 
     return True 
 
 def calculateTouchingNumbers(positions, number_positions):
-    num_list = []
-
-    print("positions: ", positions)
-    print("num pos:", number_positions)
-
-    for p in positions: 
-        #print("Position: ", p)
-        for index, num in enumerate(number_positions): 
-            #print("number_list num:", num, "index:", index)
-            for num_pos in number_positions[index][1]:
-                #print("number position: ", num_pos)
-                if num_pos == p and number_positions[index][2] != "checked": 
-                    #print("number touches a symbol")
-                    number_positions[index][2] = "checked"
-                    num_list.append(number_positions[index][0])
+    #TODO, refactor
+    for sym_index, sym in enumerate(positions):
+        for num_index, num in enumerate(number_positions): 
+            for n in num[1]: 
+                if n in sym[1] and number_positions[num_index][2] != "checked":
+                    positions[sym_index][2].append(int(number_positions[num_index][0]))
+                    number_positions[num_index][2] = "checked"
                     break 
 
-    return num_list 
 
-def parseFile(data, symbol_pos, number_pos, index, STATE, num_str, touching_pos, length):
+def parseFile(data, symbol_pos, number_pos, index, STATE, num_str, length):
     #Find symbol and number positions
-    num_indices = []
+    num_indices = {}
     for char in data: 
         #A Symbol is not a period and not a number
         if char == ".":
             if STATE == 'NUM': 
                 number_pos.append([num_str, num_indices, "unchecked"])
                 num_str = ""
-                num_indices = []
+                num_indices = {}
             STATE = 'PERIOD'
             index += 1 
             continue 
@@ -118,28 +113,19 @@ def parseFile(data, symbol_pos, number_pos, index, STATE, num_str, touching_pos,
             int(char)
             STATE = 'NUM'
             num_str += char 
-            num_indices.append(index)
+            num_indices[index] = index
             index += 1 
             continue 
         except: 
             if STATE == 'NUM': 
                 number_pos.append([num_str, num_indices, "unchecked"])
                 num_str = ""
-                num_indices = []
+                num_indices = {}
+            symbol_pos.append([char, calculateTouchingPositions(data, index, length), []])
             STATE = 'SYMBOL'
-            print("Symbol found:", char, "at index:", index)
             num_str = ""
-            symbol_pos.append(index) 
         
         index += 1 
-
-    print("Symbol positions:", symbol_pos)
-
-    #Calculate touching symbol positions
-    calculateTouchingPositions(data, symbol_pos, touching_pos, length)
-
-    print("Symbol positions:", symbol_pos)
-
 
 if __name__ == "__main__": 
     print("Accounting for Engine Parts...")
--- day3 - Gear Ratios/test.py
+++ day3 - Gear Ratios/test.py
@@ -1,4 +1,4 @@
 from counting_engine_parts import *
 
 print("Running tests....")
-print("Test 1:", countEngineParts("testdataset.txt") == 4361)
+print("Test 1:", countEngineParts("testdataset.txt") == 467835)
